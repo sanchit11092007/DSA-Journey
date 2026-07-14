d5abd3f29e9d63a4427b665f23875812f52fe123
Sanchit Goyal <sanchitgoyal11092007@gmail.com>
2026-07-14 18:12:45 +0530

    Add Code for SumOfFirstNNumbers

--- 01-Recursion/01-Basics/SumOfFirstNNumbers.java
+++ 01-Recursion/01-Basics/SumOfFirstNNumbers.java
@@ -13,7 +13,7 @@
  */
 
 
-public class SumOfFirstNumbers {
+public class SumOfFirstNNumbers {
     
     public static int sum(int n) {
         // base case 
@@ -33,3 +33,12 @@ public class SumOfFirstNumbers {
         // (5 + 4 + 3 + 2 + 1 = 15)
     }
 }
+
+/**
+ * sum(5) = 5 + sum(4)
+ *        = 5 + (4 + sum(3))
+ *        = 5 + (4 + (3 + sum(2)))
+ *        = 5 + (4 + (3 + (2 + (1 + sum(0)))))
+ *        = 5 + (4 + (3 + (2 + (1 + 0)))
+ *        = 15
+ */
